8ae05f2babef128758af88904fbe3bd7492de4c4
unknown <your.email@example.com>
2025-04-06 21:17:27 +0900

    Add initial pages directory and setup Next.js

---
+++ .next/server/pages/index.js
@@ -0,0 +1,25 @@
+(() => {
+var exports = {};
+exports.id = 405;
+exports.ids = [405];
+exports.modules = {
+
+/***/ 678:
+/***/ (() => {
+
+"function HomePage() { return <div>Welcome to Next.js!</div>; } export default HomePage;";
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(678));
+module.exports = __webpack_exports__;
+
+})();
---
+++ pages/index.js
@@ -0,0 +1,1 @@
+"function HomePage() { return <div>Welcome to Next.js!</div>; } export default HomePage;" 
